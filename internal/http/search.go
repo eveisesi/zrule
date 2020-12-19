9a7f6e13083a4b51631f019408c6309b9af99107
David Douglas <david@onetwentyseven.dev>
2020-12-19 13:28:29 -0500

    Add new search service to http server

--- internal/http/search.go
+++ internal/http/search.go
@@ -4,27 +4,51 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/eveisesi/zrule/internal/universe"
+	"github.com/eveisesi/zrule/internal/search"
 )
+
+type searchable struct {
+	Category string `json:"category"`
+	UseESI   bool   `json:"useESI"`
+	UseAPI   bool   `json:"useAPI"`
+}
 
 func (s *server) handleGetSearchCategories(w http.ResponseWriter, r *http.Request) {
 
-	var list = make([]string, len(universe.ValidSearchCategories))
-	i := 0
-	for v := range universe.ValidSearchCategories {
-		list[i] = v
-		i++
+	// Search is provided for a subset of searchable entities within Eve Online
+	// We cannot facilitate search for Alliaces, Corporations, or Characters
+	// There are just to many in Eve Online, that is why the search service only provides
+	// Regions, Constellations, Systems, Items, and ItemGroups.
+	// For Alliaces, Corporations, or Characters, we need to tell the consumer to use
+	// the actual ESI API
+
+	// There are all of the possible search categories per say
+	allCategories := []string{"alliances", "corporations", "characters", "regions", "constellations", "systems", "items", "itemGroups"}
+
+	// Get the ones that we provide
+	allKeys := search.AllKeys
+
+	results := make([]*searchable, len(allCategories))
+	for i, category := range allCategories {
+		result := new(searchable)
+		result.Category = category
+		for _, key := range allKeys {
+			if key.String() == category {
+				result.UseAPI = true
+			}
+		}
+		if !result.UseAPI {
+			result.UseESI = true
+		}
+		results[i] = result
 	}
 
-	s.writeResponse(w, http.StatusOK, list)
-
+	s.writeResponse(w, http.StatusOK, results)
 }
 
 func (s *server) handleGetSearchName(w http.ResponseWriter, r *http.Request) {
 
-	var ctx = r.Context()
-
-	user := UserFromContext(ctx)
+	user := UserFromContext(r.Context())
 	if user == nil {
 		err := fmt.Errorf("ctx does not contain a user")
 		s.logger.WithError(err).Errorln()
@@ -38,35 +62,13 @@ func (s *server) handleGetSearchName(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	category := r.URL.Query().Get("category")
-	if category == "" {
+	key := r.URL.Query().Get("key")
+	if key == "" {
 		s.writeError(w, http.StatusBadRequest, fmt.Errorf("category query paramater is required to execute a search"))
 		return
 	}
 
-	var useStrict bool
-	strict := r.URL.Query().Get("strict")
-	if strict != "" {
-		if strict == "true" {
-			useStrict = true
-		}
-	}
-
-	var valid bool
-	for i, v := range universe.ValidSearchCategories {
-		if category == i {
-			valid = true
-			category = v
-			break
-		}
-	}
-
-	if !valid {
-		s.writeError(w, http.StatusBadRequest, fmt.Errorf("invalid category %s submitted. Please use a valid category [/search/categories]", category))
-		return
-	}
-
-	results, err := s.universe.SearchName(ctx, category, term, useStrict)
+	results, err := s.search.Search(search.Key(key), term)
 	if err != nil {
 		s.writeError(w, http.StatusBadRequest, err)
 		return
